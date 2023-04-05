ef4d6218fd05c1cd23cda8b80b8941c40ae616ed
mahrezaissiouene <mahrez.aissiouene@gmail.com>
2023-04-05 02:08:00 -0400

    provide validation feedback

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -4,6 +4,7 @@ const SimpleInput = (props) => {
 
   const entrednameRef = useRef();
   const [name, setName] = useState("");
+  const [nameIsValid , setnameIsValid ] = useState(false)
 
   const changeHandler = (event) => {
     setName(event.target.value);
@@ -12,8 +13,11 @@ const SimpleInput = (props) => {
   const submitHandler = (event) => {
 
     if (name.trim() === "") {
+      setnameIsValid(false)
       return ;
     }
+
+    setnameIsValid(true)
     event.preventDefault();
     console.log("using state "+name);
 
@@ -26,6 +30,7 @@ const SimpleInput = (props) => {
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
         <input ref={entrednameRef} type="text" id="name" onChange={changeHandler} />
+     { !nameIsValid && <p className="error-text">Name must not be empty</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
